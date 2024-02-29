149870f457887b6bf46285ffda5a4b6f7a16ec99
snnh450 <159445119+snnh450@users.noreply.github.com>
2024-02-29 22:32:58 +0630

    Create foods.py

---
+++ foods.py
@@ -0,0 +1,9 @@
+from icecream import ic
+foods = ['chicken', 'pork', 'salad', 'potato']
+
+for f in foods:
+    ic(foods[1])
+    ic(foods[2])
+    ic(foods[:2])
+    ic(foods[:])
+    ic(foods[0:2])
