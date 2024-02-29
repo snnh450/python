ae2463f658f648492fc32629294d031b9a032681
snnh450 <159445119+snnh450@users.noreply.github.com>
2024-02-29 22:32:31 +0630

    Create ages.py

---
+++ ages.py
@@ -0,0 +1,8 @@
+agesbefore = [12,17,19,20]
+
+a2 = input('input the age of Tom: ')
+for a in agesbefore:
+    a2 = float(a2)
+    a3 = (2*a2) -a
+    print(f'His brothers ages are {a}')
+print(f'the curent age of Tom is {a3}')
