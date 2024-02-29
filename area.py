e98b6fdca3f386a591aefe312337abf8ca62718e
snnh450 <159445119+snnh450@users.noreply.github.com>
2024-02-29 22:31:42 +0630

    Create area.py

---
+++ area.py
@@ -0,0 +1,7 @@
+echo "# python" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/snnh450/python.git
+git push -u origin main
